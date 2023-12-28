9d61a4b6ba87aa1428c598f6905d97e75b92a8b1
Mostafa Samy <M.Mohamed@egyptianbanks.net>
2023-12-28 13:32:48 +0200

    Initial commit

---
+++ src/main/java/com/example/backtolord/BackToLordApplication.java
@@ -0,0 +1,13 @@
+package com.example.backtolord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackToLordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackToLordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backtolord/BackToLordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backtolord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackToLordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
